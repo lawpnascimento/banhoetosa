5877fd08705a1b9fe752bbd8363c836b33898a75
lawpnascimento <lawpnascimento@gmail.com>
2016-06-19 21:06:29 -0300

    Adicionado 3 campos no FAQ

--- include/view/AjudaView.php
+++ include/view/AjudaView.php
@@ -3,12 +3,39 @@
 <div class="container-fluid">
     <div class="panel panel-default">
         <div class="panel-heading">
-            <div class="panel-title">Ajuda</div>
+            <div class="panel-title">FAQ</div>
         </div>
-        <div class="panel-body" >
-          <h3>Teste titulo</h3>
-          <p>teste paragrafo resposta </p>
-        </div>
-    </div>
 
+        <div class="panel-body" >
+          <h3>Como solicitar um agendamento no Banho e Tosa?</h3>
+            <p>
+              <ul>
+                <li> Cadastrar o animal, clicando a página "Animais" no menu superior. </li>
+                <li> Cadastrar um agendamento acessando a página "Agendamentos" no menu superior.</li>
+              </ul>
+            </p>
+        </div>
+
+        <div class="panel-body" >
+          <h3>Minha solicitação ainda não foi confirmada. O que fazer?</h3>
+            <p>
+              <ul>
+                <li>Para a segurança do cliente, trabalhamos com uma confirmação via telefone.</li>
+                <li>Quando a solicitação é efetuada, seu status fica em análise.</li>
+                <li>O atendente da Banho e Tosa entrará em contato confirmando a solicitação.</li>
+                <li>Após confirmação, o agendamento é realizado com sucesso e já está tudo ok. :) </li>
+              </ul>
+           </p>
+        </div>
+
+        <div class="panel-body" >
+          <h3>Como posso alterar meu cadastro?</h3>
+            <p>
+              <ul>
+                <li>É possível alterar as informações de cadastro clicando no usuário no menu superior direito.</li>
+              </ul>
+           </p>
+        </div>
+
+    </div>
 </div>
