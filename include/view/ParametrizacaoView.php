141912f91f691858b3f140508781c98957179f40
kelvin ott <kelvinott3112@gmail.com>
2016-11-26 15:03:48 -0200

    fieldset

--- include/view/ParametrizacaoView.php
+++ include/view/ParametrizacaoView.php
@@ -9,35 +9,42 @@
         </div>
         <div class="panel-body" >
             <form id="parametrizacaoForm" class="form-horizontal" role="form">
-                <div class="form-group">
-                    <label for="nome" class="col-md-1 control-label">Nome</label>
-                    <div class="col-md-4">
-                        <input id="txbEmpresa" type="text" class="form-control" name="txbEmpresa" placeholder="Empresa"></input>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="horario" class="col-md-1 control-label">Horário</label>
-                    <div class="col-md-2">
-                        <input id="txbHorarioDe" type="text" class="form-control" name="txbHorarioDe" placeholder="De"/>
-                    </div>
-                    <div class="col-md-2">
-                        <input id="txbHorarioAte" type="text" class="form-control" name="txbHorarioAte" placeholder="Até"/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="usuarios" class="col-md-1 control-label">Usuário</label>
-                    <div class="col-md-4">
-                        <div class="dropdown">
-                            <button class="btn btn-default dropdown-toggle form-control" type="button" id="ddlUsuario" data-toggle="dropdown" aria-expanded="true" name="usuarios">
-                                Usuários
-                                <span class="caret"></span>
-                            </button>
-                            <ul id="ulUsuario" class="dropdown-menu" role="menu" aria-labelledby="ddlUsuario">
+                <fieldset>
+                    <legend class="scheduler-border">Empresa</legend>
 
-                            </ul>
+                    <div class="form-group">
+                        <label for="nome" class="col-md-1 control-label">Nome</label>
+                        <div class="col-md-4">
+                            <input id="txbEmpresa" type="text" class="form-control" name="txbEmpresa" placeholder="Empresa"></input>
                         </div>
                     </div>
-                </div>
+                    <div class="form-group">
+                        <label for="horario" class="col-md-1 control-label">Horário</label>
+                        <div class="col-md-2">
+                            <input id="txbHorarioDe" type="text" class="form-control" name="txbHorarioDe" placeholder="De"/>
+                        </div>
+                        <div class="col-md-2">
+                            <input id="txbHorarioAte" type="text" class="form-control" name="txbHorarioAte" placeholder="Até"/>
+                        </div>
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <legend class="scheduler-border">Usuários</legend>
+                    <div class="form-group">
+                        <label for="usuarios" class="col-md-1 control-label">Usuário</label>
+                        <div class="col-md-4">
+                            <div class="dropdown">
+                                <button class="btn btn-default dropdown-toggle form-control" type="button" id="ddlUsuario" data-toggle="dropdown" aria-expanded="true" name="usuarios">
+                                    Usuários
+                                    <span class="caret"></span>
+                                </button>
+                                <ul id="ulUsuario" class="dropdown-menu" role="menu" aria-labelledby="ddlUsuario">
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </fieldset>
                 <div class="form-group">
                     <label for="perfil" class="col-md-1 control-label">Perfil</label>
                     <div class="col-md-4">
